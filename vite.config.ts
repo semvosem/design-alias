ac1ca21387edea6cfca3d3aec5d320b8ce933d5c
semvosem <semvosem@users.noreply.github.com>
2025-10-22 11:56:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
-import observerPlugin from "mobx-react-observer/babel-plugin";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [observerPlugin()],
-      }
-    }), 
-    VitePWA({
-      registerType: 'autoUpdate',
-      // devOptions: {
-      //   enabled: true,
-      // },
-      manifest: {
-        name: 'Alias game',
-        theme_color: '#e7d0a4',
-      }
-    })
-  ],
-  base: '/alias/',
+  base: "/design-alias/", // <- обязательно имя репозитория
+  plugins: [react()],
 });
